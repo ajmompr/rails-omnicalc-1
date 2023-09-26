4e90b8e5655d4bfb431fa9c1c4e296027bd3df25
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 17:10:02 +0000

    added payment/new & payment/results routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
 
   get("/square_root/new", { :controller => "pages", :action => "square_root"})
   get("/square_root/results", { :controller => "pages", :action => "square_root_results"})
+
+  get("/payment/new", { :controller => "pages", :action => "payment"})
+  get("/payment/results", { :controller => "pages", :action => "payment_results"})
   
 end
