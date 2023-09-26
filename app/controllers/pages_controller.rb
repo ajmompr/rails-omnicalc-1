0952dab1ec724512893c9d89c3a1402277dde8f8
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 16:36:19 +0000

    defined square_results action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def square
     render({ :template => "page_templates/square"})
   end
+
+  def square_results
+    render({ :template => "page_templates/square_results"})
+  end
 end
