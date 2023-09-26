e23516b495d0bdf52eb46427171efecb0161e27d
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 16:30:13 +0000

    added root route and square/results route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "pages", :action => "square" })
+  post("/square/results", { :controller => "pages", :action => "square_results"})
   
 end
