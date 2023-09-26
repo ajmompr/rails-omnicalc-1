db1cdf2c2b428b5d442169cb103d4d12ea9b41e5
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 16:46:59 +0000

    added ruby statemetns to square_results action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,9 @@ class PagesController < ApplicationController
   end
 
   def square_results
+    @user_num = params.fetch("user_number").to_f
+    @square_value = @user_num**2
+
     render({ :template => "page_templates/square_results"})
   end
 end
