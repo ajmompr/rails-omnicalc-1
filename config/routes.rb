0c3f7daa97577d5da554d13fad80f3472a45ccc6
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 17:02:45 +0000

    changed square & sqr_rt results routes to get method

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get("/", { :controller => "pages", :action => "square" })
 
   get("/square/new", { :controller => "pages", :action => "square" })
-  post("/square/results", { :controller => "pages", :action => "square_results"})
+  get("/square/results", { :controller => "pages", :action => "square_results"})
 
   get("/square_root/new", { :controller => "pages", :action => "square_root"})
-  post("/square_root/results", { :controller => "pages", :action => "square_root_results"})
+  get("/square_root/results", { :controller => "pages", :action => "square_root_results"})
   
 end
