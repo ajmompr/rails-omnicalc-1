2c5cb7fcaa7537d0e65f53fb316a993efac645fd
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 16:43:32 +0000

    added square/new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "pages", :action => "square" })
+  get("/square/new", { :controller => "pages", :action => "square" })
   post("/square/results", { :controller => "pages", :action => "square_results"})
   
 end
