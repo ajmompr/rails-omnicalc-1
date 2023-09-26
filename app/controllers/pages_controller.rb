6a9dd049e7b1bb82b510ab46dc1923b9fa64d56b
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 17:14:52 +0000

    added render method to payment & payment_resutls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,29 +1,31 @@
 class PagesController < ApplicationController
   def square
-    render({ :template => "page_templates/square"})
+    render({ :template => "page_templates/square" })
   end
 
   def square_results
     @user_num = params.fetch("user_number").to_f
     @square_value = @user_num**2
 
-    render({ :template => "page_templates/square_results"})
+    render({ :template => "page_templates/square_results" })
   end
 
   def square_root
-    render({ :template => "page_templates/square_root"})
+    render({ :template => "page_templates/square_root" })
   end
 
   def square_root_results
     @user_num = params.fetch("user_number").to_f
     @sqr_root = @user_num**(0.5)
 
-    render({ :template => "page_templates/square_root_results"})
+    render({ :template => "page_templates/square_root_results" })
   end
 
   def payment
+    render({ :template => "page_templates/payment" })
   end
 
   def payment_results
+    render({:template => "page_templates/payment_results"})
   end
 end
